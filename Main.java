916c4976517f564e191ffb8f1a1e5cdd8b666dcb
LAB4804WD29 <W7415979@student.miracosta.edu>
2026-06-25 10:12:03 -0700

    finished project

--- Main.java
+++ Main.java
@@ -10,9 +10,24 @@ public class Main
 	{
 		System.out.println("My thoughts on the article are:");
 		// Part 2: Invoking/calling method
+		Main.selfDrivingOpinions();
 		
 	}
-	
+
 	// Part 1: Creating method definition
+	// method header:
+	public static void selfDrivingOpinions()
+	{
+		// return type: void
+		// method identifier selfDrivingOpinions
+		// method body:
+		System.out.println("");
+		System.out.println("I think a self driving car should try to protect everyone involved rather than automatically choosing passengers or pedestrians. Its main goal should be to reduce the overall risk of injury and the number of fatalities.");
+		System.out.println("");
+		System.out.println("The algorithms should consider the number of people involved, the likelihood and severity of injuries, speed, and whether there are any safe ways to avoid the crash.");
+		System.out.println("");
+		System.out.println("The algorithms should be decided by engineers. safety experts, government regulators working together. This would help ensure that the cars are safe, fair, and follow the standards.");
+
+	}
 	
 }
